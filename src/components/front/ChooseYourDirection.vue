5d0232a4613b3deea14512656ae9c8ed4c421f1f
Eduard Riazantcev <ed.ryazancev@paysenger.com>
2025-02-24 19:38:32 +0700

    mobile choose your direction

--- src/components/front/ChooseYourDirection.vue
+++ src/components/front/ChooseYourDirection.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container mt-[120px] py-[120px]">
-    <div class="flex gap-10 px-[96px]">
+  <div class="container py-[72px] lg:mt-[120px] lg:py-[120px]">
+    <div class="flex gap-6 max-lg:flex-col lg:gap-10 lg:px-[96px]">
       <div class="flex-1">
         <div class="h2">
           {{ $t('choose') }}
@@ -12,17 +12,19 @@
         <div class="p-s mt-6">{{ $t('hyperspace_offers') }}</div>
       </div>
 
-      <div class="flex min-w-0 flex-1 flex-col gap-4">
-        <NuxtLink><img src="/assets/images/choose/images.webp" alt="" class="w-full" /></NuxtLink>
-        <NuxtLink><img src="/assets/images/choose/chatbots.webp" alt="" class="w-full" /></NuxtLink>
-        <NuxtLink><img src="/assets/images/choose/texts.webp" alt="" class="w-full" /></NuxtLink>
-        <NuxtLink><img src="/assets/images/choose/music.webp" alt="" class="w-full" /></NuxtLink>
-        <NuxtLink><img src="/assets/images/choose/voices.webp" alt="" class="w-full" /></NuxtLink>
+      <div class="flex min-w-0 flex-1 flex-col gap-2 lg:gap-4">
+        <NuxtLink v-for="([domain], index) in domains" :key="index" :to="domain.to">
+          <img
+            :src="`/assets/images/choose/${domain.label?.toLowerCase?.()}.webp`"
+            alt=""
+            class="w-full"
+          />
+        </NuxtLink>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import CatalogPreview from '~/components/catalog/CatalogPreview.vue'
+const domains = useState('domains', () => [])
 </script>
--- src/components/front/Reviews.vue
+++ src/components/front/Reviews.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="mt-[200px] bg-white pt-[144px]">
+  <div class="mt-[72px] bg-white py-[72px] lg:mt-[200px]">
     <div class="mx-auto flex max-w-[1086px] justify-between">
-      <div class="h2 max-w-[698px]">
+      <div class="h2 max-w-[698px] px-6">
         {{ $t('dont') }}
         <span class="text-gradient">{{ $t('users') }}</span>
       </div>
 
-      <div class="flex gap-5">
+      <div class="flex gap-5 max-lg:hidden">
         <button
           style="outline: 0"
           class="flex h-14 w-14 items-center justify-center rounded-full bg-black-100"
@@ -22,8 +22,23 @@
       </div>
     </div>
 
-    <div class="flex mt-12 min-w-0 items-start gap-4 overflow-x-auto">
+    <div class="mt-12 flex min-w-0 items-start gap-4 overflow-x-auto">
       <ReviewItem v-for="review in reviews" :key="review.id" v-bind="review" />
+    </div>
+
+    <div class="mt-6 flex justify-center gap-5 lg:hidden">
+      <button
+        style="outline: 0"
+        class="flex h-10 w-10 items-center justify-center rounded-full bg-black-100"
+      >
+        <GIcon name="icon_left" />
+      </button>
+      <button
+        style="outline: 0"
+        class="flex h-10 w-10 items-center justify-center rounded-full bg-black-100"
+      >
+        <GIcon name="icon_right" />
+      </button>
     </div>
   </div>
 </template>
